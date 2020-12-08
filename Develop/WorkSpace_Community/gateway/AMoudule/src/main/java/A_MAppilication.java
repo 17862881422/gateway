156267a198eaae093e449afde8010205a9de592b
17862881422 <1169192618@qq.com>
2020-12-08 09:57:05 +0800

    11

---
+++ Develop/WorkSpace_Community/gateway/AMoudule/src/main/java/A_MAppilication.java
@@ -0,0 +1,11 @@
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class A_MAppilication {
+    public static void main(String[] args) {
+        SpringApplication.run(A_MAppilication.class,args);
+    }
+}
+
